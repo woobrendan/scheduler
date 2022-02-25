84a60b61088ac99face38eef55346ec492d98df0
Brendan Woo <woo.brendan@gmail.com>
2022-02-25 17:36:14 -0500

    remove commented out line

--- src/components/Application.js
+++ src/components/Application.js
@@ -23,7 +23,6 @@ export default function Application(props) {
       axios.get('/api/appointments'),
       axios.get('/api/interviewers')
     ]).then((all) => {
-      // const [daysArray, appointmentsArray] = all;
       setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}))
     })
   },[])
