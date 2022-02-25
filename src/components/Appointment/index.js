fee8133904d604d034dc552951772c9c09ad193a
Brendan Woo <woo.brendan@gmail.com>
2022-02-25 17:37:14 -0500

    add custom hook and implement it for various components being shown

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,16 +3,32 @@ import 'components/Appointment/styles.scss';
 import Header from '../Appointment/header';
 import Show from './show';
 import Empty from './empty';
+import Form from './Form';
+import useVisualMode from "hooks/useVisualMode";
 
 
 export default function Appointment(props) {
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+  const {mode , transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (
     <article className="appointment">
       <Header time={props.time}/>
-      {props.interview ? 
-      <Show student={props.interview.student} 
-        interviewer={props.interview.interviewer}/> 
-      : <Empty />}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)}/>}
+      {mode === SHOW && (
+        <Show 
+          student={props.interview.student} 
+          interviewer={props.interview.interviewer} 
+        />)}
+      {mode === CREATE && (
+        <Form 
+          interviewers={[]} 
+          onCancel={back}
+        />)}
     </article>
   );
 }
