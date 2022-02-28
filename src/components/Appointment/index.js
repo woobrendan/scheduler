79a71ad9f88da2a9a92f6cb92f396cfd3e284f31
Brendan Woo <woo.brendan@gmail.com>
2022-02-28 16:13:09 -0500

    add save function with bookIntereview prop. add saving component and mode

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,21 +4,37 @@ import Header from '../Appointment/header';
 import Show from './show';
 import Empty from './empty';
 import Form from './Form';
+import Status from './Status';
 import useVisualMode from "hooks/useVisualMode";
+
 
 
 export default function Appointment(props) {
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
   const CREATE = "CREATE";
+  const SAVING = "SAVING";
   const {mode , transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
-
+  function save(name, interviewer) {
+    const interview = {
+      student: name,
+      interviewer
+    };
+    transition(SAVING);
+    props.bookInterview(props.id, interview)
+      .then(() => {
+        transition(SHOW);
+      })
+    .catch(err => console.log("error:", err))
+  }
+ 
   return (
     <article className="appointment">
       <Header time={props.time}/>
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)}/>}
+      {mode === SAVING && <Status />}
       {mode === SHOW && (
         <Show 
           student={props.interview.student} 
@@ -26,8 +42,9 @@ export default function Appointment(props) {
         />)}
       {mode === CREATE && (
         <Form 
-          interviewers={[]} 
+          interviewers={props.interviewers} 
           onCancel={back}
+          onSave={save}
         />)}
     </article>
   );
