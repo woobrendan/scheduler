73dfc78bcf68dc45a1b4e3b8765560f65d421ea7
Brendan Woo <woo.brendan@gmail.com>
2022-03-06 17:27:04 -0500

    add logic to ensure interviewer is selected

--- src/components/Appointment/Form.jsx
+++ src/components/Appointment/Form.jsx
@@ -23,6 +23,10 @@ export default function Form(props) {
       setError("Student name cannot be blank");
       return;
     }
+    if (!interviewer) {
+      setError("Interviewer must be selected");
+      return;
+    }
     setError("");
     props.onSave(student, interviewer);
   };
