33d191126683305c2866b8ad260f0b3223531b6f
Brendan Woo <woo.brendan@gmail.com>
2022-03-06 17:20:26 -0500

    add comments

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,6 +4,7 @@ export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
+  //add new mode to history. if replace is true, remove the last element then add new mode
   const transition = (newMode, replace = false) => {
     const copyHistory = [...history];
     setMode(newMode);
@@ -16,6 +17,7 @@ export default function useVisualMode(initial) {
     }
     setHistory(copyHistory);
   }
+  //returns visual mode to previous state
   const back = () => {
     const copyHistory = [...history];
     if (history.length === 1) {
